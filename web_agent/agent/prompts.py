fab1c87deaee070c350a854bebe2467869a9d3ba
Niklas Simakov <niklas.simakov@tum.de>
2025-06-10 01:26:13 +0200

    refactor: remove unused actions prompt

--- web_agent/agent/prompts.py
+++ web_agent/agent/prompts.py
@@ -1,28 +1,3 @@
-def get_possible_actions_prompt() -> str:
-	"""
-	Get a prompt that lists all possible actions that the agent can take.
-	"""
-
-	# TODO: move this to the actions controller and remove this function
-
-	# BACKUP FOR EASY COPYING
-	backup = """
-- "scroll('direction')": Scroll the page in the given direction. Valid directions are 'up', 'down'.
-	"""  # noqa: F841
-
-	return """
-- "click_text('text')": Click on the element that contains the given text. If multiple possible elements are found, try using the click_text_ith() method.
-- "click_text_ith('text', 'i')": Click on the ith element that contains the given text. Both arguments are strings.
-- "click_coord('x', 'y')": Click on the element at the given coordinates.
-- "scroll('direction')": Scroll the page in the given direction. Valid directions are 'up', 'down'.
-- "search('query')": Search for the given query on the current page and focus on it.
-- "type('text')": Type the given text into the focused element.
-- "fill('placeholder', 'input')": Fill the given input text into the first element that has the given placeholder text.
-- "back('')": Go back to the previous page.
-- "reset('')": Reset the browser to the initial starting page.
-		"""  # noqa: E501
-
-
 def get_system_prompt() -> str:
 	return """
 You are a goal-driven web agent. Your objective is to complete the given task by interacting with the current webpage, based on its screenshot, metadata, and the history of your previous actions and their results.
@@ -57,4 +32,4 @@ Do not output anything besides the structured JSON object.
   "action": "click_text('Log in')"
 }
 ```
-		"""
+"""
