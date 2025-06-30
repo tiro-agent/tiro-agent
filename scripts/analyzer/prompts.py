5a7ebb1e37bc7e4a6d3901c1ca5f38208155eb97
Niklas Simakov <niklas.simakov@tum.de>
2025-06-30 22:35:53 +0200

    feat: add AI error evaluation to analysis script
    - Add new dependencies: pandera and tqdm.
    - Removed the old analysis script and replaced it with a new implementation in a different

---
+++ scripts/analyzer/prompts.py
@@ -0,0 +1,29 @@
+def get_ai_eval_prompt() -> str:
+	return """
+You are a smart and analytical assistant that evaluates why a web agent failed to complete a task.
+
+The possible causes are:
+- OPTION_SELECTION_ERROR: the agent was unable to select an option from a dropdown or a list
+- FILTER_ERROR: the agent was unable use a search filter or the filter was not found or applied wrong
+- CLICK_ERROR: the agent was unable to click on the element or the element was not found or clicked on the wrong element
+- NAVIGATION_ERROR: the agent couldn't navigate to the correct page or the page was not found
+- SCROLL_ERROR: the agent was unable to scroll or scrolled infinetly whitout finding the element
+- INPUT_ERROR: the agent was unable to input text or the text was not found or inputted in the wrong field
+- OTHER: the agent was unable to complete the task for other reasons (only use this if none of the other causes apply)
+
+You are given a task and steps that the agent took to complete the task (until step limit was reached), including the screenshots of the last few steps.
+You need to determine what the agent was unable to do.
+
+You need to return the thought process of the evaluation and the cause of the failure.
+Evaluate all possible causes, whitout jumping to conclusions. Decide at the end.
+
+Example:
+{
+	"thought_process": "The action history clearly shows that the agent was unable to click on the required element. This is also supported by the screenshot of the last few steps.",
+	"cause": "CLICK_ERROR"
+}
+"""  # noqa: E501
+
+
+if __name__ == '__main__':
+	print(get_ai_eval_prompt())
