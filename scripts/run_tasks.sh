d4338857ef1cd85267db2d178fb4a6ce5ad69e2b
Niklas Simakov <niklas.simakov@tum.de>
2025-07-09 23:46:32 +0200

    refactor: update run script to use the build in vpn check

--- scripts/run_tasks.sh
+++ scripts/run_tasks.sh
@@ -3,18 +3,10 @@
 # set run id
 run_id="try_multi_screenshot_1"
 
-# make sure the vpn is on, by prompting the user and he must reply "y"
-read -p "Please turn on the vpn and reply 'y' to continue: "
-if [ "$REPLY" != "y" ]; then
-    echo "VPN is not on, please turn it on and run the script again"
-    exit 1
-fi
-
 uv run -m web_agent \
 --logfire \
 --level easy \
---run-id $run_id \
---disable-vpn-check
+--run-id $run_id
 
 uv run -m web_agent \
 --logfire \
